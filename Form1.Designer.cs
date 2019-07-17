3102a9b7f21aeeb288f4da4c26a3a4267b2d4193
andreahegedus <43707028+andreahegedus@users.noreply.github.com>
2019-07-17 16:14:37 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Calculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one_button = new System.Windows.Forms.Button();
+            this.szorzas_button = new System.Windows.Forms.Button();
+            this.two_button = new System.Windows.Forms.Button();
+            this.osszeadas_button = new System.Windows.Forms.Button();
+            this.kivonas_button = new System.Windows.Forms.Button();
+            this.three_button = new System.Windows.Forms.Button();
+            this.six_button = new System.Windows.Forms.Button();
+            this.five_button = new System.Windows.Forms.Button();
+            this.four_button = new System.Windows.Forms.Button();
+            this.osztas_button = new System.Windows.Forms.Button();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.egyenlo_button = new System.Windows.Forms.Button();
+            this.nine_button = new System.Windows.Forms.Button();
+            this.eight_button = new System.Windows.Forms.Button();
+            this.seve_button = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.zero_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // one_button
+            // 
+            this.one_button.Location = new System.Drawing.Point(12, 164);
+            this.one_button.Name = "one_button";
+            this.one_button.Size = new System.Drawing.Size(75, 63);
+            this.one_button.TabIndex = 1;
+            this.one_button.Text = "1";
+            this.one_button.UseVisualStyleBackColor = true;
+            this.one_button.Click += new System.EventHandler(this.One_button_Click);
+            // 
+            // szorzas_button
+            // 
+            this.szorzas_button.Location = new System.Drawing.Point(205, 87);
+            this.szorzas_button.Name = "szorzas_button";
+            this.szorzas_button.Size = new System.Drawing.Size(75, 63);
+            this.szorzas_button.TabIndex = 1;
+            this.szorzas_button.Text = "*";
+            this.szorzas_button.UseVisualStyleBackColor = true;
+            this.szorzas_button.Click += new System.EventHandler(this.Szorzas_button_Click);
+            // 
+            // two_button
+            // 
+            this.two_button.Location = new System.Drawing.Point(105, 164);
+            this.two_button.Name = "two_button";
+            this.two_button.Size = new System.Drawing.Size(75, 63);
+            this.two_button.TabIndex = 1;
+            this.two_button.Text = "2";
+            this.two_button.UseVisualStyleBackColor = true;
+            this.two_button.Click += new System.EventHandler(this.Two_button_Click);
+            // 
+            // osszeadas_button
+            // 
+            this.osszeadas_button.Location = new System.Drawing.Point(12, 87);
+            this.osszeadas_button.Name = "osszeadas_button";
+            this.osszeadas_button.Size = new System.Drawing.Size(75, 58);
+            this.osszeadas_button.TabIndex = 1;
+            this.osszeadas_button.Text = "+";
+            this.osszeadas_button.UseVisualStyleBackColor = true;
+            this.osszeadas_button.Click += new System.EventHandler(this.Osszeadas_button_Click);
+            // 
+            // kivonas_button
+            // 
+            this.kivonas_button.Location = new System.Drawing.Point(105, 87);
+            this.kivonas_button.Name = "kivonas_button";
+            this.kivonas_button.Size = new System.Drawing.Size(75, 58);
+            this.kivonas_button.TabIndex = 1;
+            this.kivonas_button.Text = "-";
+            this.kivonas_button.UseVisualStyleBackColor = true;
+            this.kivonas_button.Click += new System.EventHandler(this.Kivonas_button_Click);
+            // 
+            // three_button
+            // 
+            this.three_button.Location = new System.Drawing.Point(205, 164);
+            this.three_button.Name = "three_button";
+            this.three_button.Size = new System.Drawing.Size(75, 63);
+            this.three_button.TabIndex = 1;
+            this.three_button.Text = "3";
+            this.three_button.UseVisualStyleBackColor = true;
+            this.three_button.Click += new System.EventHandler(this.Three_button_Click);
+            // 
+            // six_button
+            // 
+            this.six_button.Location = new System.Drawing.Point(205, 238);
+            this.six_button.Name = "six_button";
+            this.six_button.Size = new System.Drawing.Size(75, 63);
+            this.six_button.TabIndex = 1;
+            this.six_button.Text = "6";
+            this.six_button.UseVisualStyleBackColor = true;
+            this.six_button.Click += new System.EventHandler(this.Six_button_Click);
+            // 
+            // five_button
+            // 
+            this.five_button.Location = new System.Drawing.Point(105, 238);
+            this.five_button.Name = "five_button";
+            this.five_button.Size = new System.Drawing.Size(75, 63);
+            this.five_button.TabIndex = 1;
+            this.five_button.Text = "5";
+            this.five_button.UseVisualStyleBackColor = true;
+            this.five_button.Click += new System.EventHandler(this.Five_button_Click);
+            // 
+            // four_button
+            // 
+            this.four_button.Location = new System.Drawing.Point(12, 238);
+            this.four_button.Name = "four_button";
+            this.four_button.Size = new System.Drawing.Size(75, 63);
+            this.four_button.TabIndex = 1;
+            this.four_button.Text = "4";
+            this.four_button.UseVisualStyleBackColor = true;
+            this.four_button.Click += new System.EventHandler(this.Four_button_Click);
+            // 
+            // osztas_button
+            // 
+            this.osztas_button.Location = new System.Drawing.Point(292, 87);
+            this.osztas_button.Name = "osztas_button";
+            this.osztas_button.Size = new System.Drawing.Size(75, 63);
+            this.osztas_button.TabIndex = 1;
+            this.osztas_button.Text = "/";
+            this.osztas_button.UseVisualStyleBackColor = true;
+            this.osztas_button.Click += new System.EventHandler(this.Osztas_button_Click);
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(292, 164);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(75, 63);
+            this.clear_button.TabIndex = 1;
+            this.clear_button.Text = "C";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.Clear_button_Click);
+            // 
+            // egyenlo_button
+            // 
+            this.egyenlo_button.Location = new System.Drawing.Point(292, 238);
+            this.egyenlo_button.Name = "egyenlo_button";
+            this.egyenlo_button.Size = new System.Drawing.Size(75, 199);
+            this.egyenlo_button.TabIndex = 1;
+            this.egyenlo_button.Text = "=";
+            this.egyenlo_button.UseVisualStyleBackColor = true;
+            this.egyenlo_button.Click += new System.EventHandler(this.Egyenlo_button_Click);
+            // 
+            // nine_button
+            // 
+            this.nine_button.Location = new System.Drawing.Point(205, 307);
+            this.nine_button.Name = "nine_button";
+            this.nine_button.Size = new System.Drawing.Size(75, 63);
+            this.nine_button.TabIndex = 1;
+            this.nine_button.Text = "9";
+            this.nine_button.UseVisualStyleBackColor = true;
+            this.nine_button.Click += new System.EventHandler(this.Nine_button_Click);
+            // 
+            // eight_button
+            // 
+            this.eight_button.Location = new System.Drawing.Point(105, 307);
+            this.eight_button.Name = "eight_button";
+            this.eight_button.Size = new System.Drawing.Size(75, 63);
+            this.eight_button.TabIndex = 1;
+            this.eight_button.Text = "8";
+            this.eight_button.UseVisualStyleBackColor = true;
+            this.eight_button.Click += new System.EventHandler(this.Eight_button_Click);
+            // 
+            // seve_button
+            // 
+            this.seve_button.Location = new System.Drawing.Point(12, 307);
+            this.seve_button.Name = "seve_button";
+            this.seve_button.Size = new System.Drawing.Size(75, 63);
+            this.seve_button.TabIndex = 1;
+            this.seve_button.Text = "7";
+            this.seve_button.UseVisualStyleBackColor = true;
+            this.seve_button.Click += new System.EventHandler(this.Seve_button_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.16F);
+            this.TextBox.Location = new System.Drawing.Point(12, 12);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(355, 45);
+            this.TextBox.TabIndex = 2;
+            this.TextBox.Text = "";
+            // 
+            // zero_button
+            // 
+            this.zero_button.Location = new System.Drawing.Point(12, 376);
+            this.zero_button.Name = "zero_button";
+            this.zero_button.Size = new System.Drawing.Size(268, 61);
+            this.zero_button.TabIndex = 3;
+            this.zero_button.Text = "0";
+            this.zero_button.UseVisualStyleBackColor = true;
+            this.zero_button.Click += new System.EventHandler(this.Zero_button_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 448);
+            this.Controls.Add(this.zero_button);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.kivonas_button);
+            this.Controls.Add(this.five_button);
+            this.Controls.Add(this.two_button);
+            this.Controls.Add(this.six_button);
+            this.Controls.Add(this.three_button);
+            this.Controls.Add(this.osztas_button);
+            this.Controls.Add(this.seve_button);
+            this.Controls.Add(this.eight_button);
+            this.Controls.Add(this.nine_button);
+            this.Controls.Add(this.egyenlo_button);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.szorzas_button);
+            this.Controls.Add(this.osszeadas_button);
+            this.Controls.Add(this.four_button);
+            this.Controls.Add(this.one_button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button one_button;
+        private System.Windows.Forms.Button szorzas_button;
+        private System.Windows.Forms.Button two_button;
+        private System.Windows.Forms.Button osszeadas_button;
+        private System.Windows.Forms.Button kivonas_button;
+        private System.Windows.Forms.Button three_button;
+        private System.Windows.Forms.Button six_button;
+        private System.Windows.Forms.Button five_button;
+        private System.Windows.Forms.Button four_button;
+        private System.Windows.Forms.Button osztas_button;
+        private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Button egyenlo_button;
+        private System.Windows.Forms.Button nine_button;
+        private System.Windows.Forms.Button eight_button;
+        private System.Windows.Forms.Button seve_button;
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button zero_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        public double elsoszam;
+        public string op;
+        public char save_mjel = ' ';
+        public double save_szam = 0;
+        public double eredmeny = 0;
+        public Calculator()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void One_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "1";
+        }
+
+        private void Two_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "2";
+
+        }
+
+        private void Three_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "3";
+        }
+
+        private void Four_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "4";
+        }
+
+        private void Five_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "5";
+        }
+
+        private void Six_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "6";
+        }
+
+        private void Seve_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "7";
+        }
+
+        private void Eight_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "8";
+        }
+
+        private void Nine_button_Click(object sender, EventArgs e)
+        {
+            TextBox.Text += "9";
+        }
+
+        private void Zero_button_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text.Length > 0)
+            {
+                TextBox.Text += "0";
+            }
+        }
+
+        private void Osszeadas_button_Click(object sender, EventArgs e)
+        {
+            save_szam = double.Parse(TextBox.Text);
+            eredmeny += save_szam;
+            save_mjel = '+';
+            TextBox.Text = "";
+            
+        }
+
+        private void Kivonas_button_Click(object sender, EventArgs e)
+        {
+            save_szam = double.Parse(TextBox.Text);
+            eredmeny -= save_szam;
+            save_mjel = '-';
+            TextBox.Text = "";
+        }
+
+        private void Szorzas_button_Click(object sender, EventArgs e)
+        {
+            save_szam = double.Parse(TextBox.Text);
+            if (eredmeny == 0)
+            {
+                eredmeny = save_szam;
+            }
+            else
+            {
+                eredmeny = eredmeny * save_szam;
+            }
+            save_mjel = '*';
+            TextBox.Text = "";
+           
+        }
+
+        private void Osztas_button_Click(object sender, EventArgs e)
+        {
+            save_szam = double.Parse(TextBox.Text);
+            eredmeny = eredmeny / save_szam;
+            save_mjel = '/';
+            TextBox.Text = "";
+        }
+
+        private void Clear_button_Click(object sender, EventArgs e)
+        {
+            
+            TextBox.Text = "";
+            save_mjel = ' ';
+            save_szam = 0;
+
+        }
+
+        private void Egyenlo_button_Click(object sender, EventArgs e)
+        {
+            save_szam = double.Parse(TextBox.Text);
+            switch (save_mjel)
+            {
+                case '+':
+                    eredmeny = eredmeny + save_szam;
+                    break;
+                case '-':
+                    eredmeny = eredmeny - save_szam;
+                    break;
+                case '*':
+                    eredmeny = eredmeny * save_szam;
+                    break;
+                case '/':
+                    eredmeny = eredmeny / save_szam;
+                    break;
+            }
+            TextBox.Text = Convert.ToString(eredmeny);
+            eredmeny = 0;
+            save_mjel = ' ';
+            save_szam = 0;
+        }
+    }
+}
+    
+    
+
